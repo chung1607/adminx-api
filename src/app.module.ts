bd9bca8037d3b24491b89a64a999f75d3a67de36
LaiVanChung <126126468+tothichcau02@users.noreply.github.com>
2025-07-07 14:23:58 +0700

    added file .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataSourceOptions } from 'db/data-source';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(dataSourceOptions),],
+  imports: [
+    TypeOrmModule.forRoot(dataSourceOptions),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
